5db9ae0f38099e2bdb537d8ac06ebcc9388f4dea
rexxis <david.harendza@gmail.com>
2022-07-14 12:19:27 +0700

    test board print

--- tictactoe.py
+++ tictactoe.py
@@ -2,3 +2,16 @@ print("This is a Tic Tac Toe terminal game")
 print("Created for Codecademy CS 101 Portfolio")
 print("")
 print("David Harendza - July 2022")
+
+class TicTacToe:
+    map = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+    def __init__(self):
+        self.board = []
+
+    def printboard(self):
+        print(TicTacToe.map[:3])
+        print(TicTacToe.map[4:6])
+        print(TicTacToe.map[7:])
+
+instance1 = TicTacToe()
+instance1.printboard

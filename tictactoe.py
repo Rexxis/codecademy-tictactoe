314b3e12d09c050f72b77700a87bb01628549acf
David <davidhrz@H7-PC.localdomain>
2022-09-21 14:52:07 +0700

    change month to september

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,7 @@
 print("This is a Tic Tac Toe terminal game")
 print("Created for Codecademy CS 101 Portfolio")
 print("")
-print("David Harendza - July 2022")
+print("David Harendza - September 2022")
 
 class TicTacToe:
 

2de3974c54bfa7e08de5da0df97ed11361db23df
rexxis <david.harendza@gmail.com>
2022-09-21 17:13:55 +0700

    game running with guide

--- tictactoe.py
+++ tictactoe.py
@@ -5,9 +5,11 @@ print("David Harendza - September 2022")
 print("")
 
 winner = None
+gameRunning = None
+currentPlayer = "X"
 
 class Player:
-    def __init__(self, player):
+    def __init__(self, player="X"):
         self.player = player
 
 class TicTacToe:
@@ -16,6 +18,7 @@ class TicTacToe:
         self.board = ["-","-","-","-","-","-","-","-","-"]
 
     def printboard(self):
+        print("")
         print("BOARD POSITION")
         print(self.board[0], " | ", self.board[1], " | ", self.board[2])
         print("---------------")
@@ -25,17 +28,11 @@ class TicTacToe:
         print("---------------")
     
     def playerInput(self):
-        inp0 = input("x or o player ? (enter x or o): ")
-        if inp0 == "x":
-            self.player = "x"
-        elif inp0 == "o":
-            self.player = "o"
-        else:
-            print("Please enter x or o!")
-
+        global currentPlayer
+        print(f"Player {currentPlayer} turn")
         inp = int(input("Choose your position (1-9): "))
         if inp >= 1 and inp <= 9 and self.board[inp-1] == "-":
-            self.board[inp-1] = self.player
+            self.board[inp-1] = currentPlayer
         else:
             print("Other player is already in the position. Choose other position!")
 
@@ -50,14 +47,78 @@ class TicTacToe:
         elif self.board[6] == self.board[7] == self.board[8] and self.board[6] != "-":
             winner = self.board[6]
             return True
+    
+    def checkRow(self):
+        global winner
+        if self.board[0] == self.board[3] == self.board[6] and self.board[0] != "-":
+            winner = self.board[0]
+            return True
+        if self.board[1] == self.board[4] == self.board[7] and self.board[1] != "-":
+            winner = self.board[1]
+            return True
+        if self.board[2] == self.board[5] == self.board[8] and self.board[2] != "-":
+            winner = self.board[2]
+            return True
+    
+    def checkDiag(self):
+        global winner
+        if self.board[0] == self.board[4] == self.board[8] and self.board[0] != "-":
+            winner = self.board[0]
+            return True
+        if self.board[2] == self.board[4] == self.board[6] and self.board[2] != "-":
+            winner = self.board[2]
+            return True
+    
+    def checkTie(self):
+        global gameRunning
+        if "-" not in self.board:
+            self.printboard()
+            print("It's a tie!")
+            gameRunning = False
 
+    def switchPlayer(self):
+        global currentPlayer
+        if currentPlayer == "X":
+            currentPlayer = "O"
+        else:
+            currentPlayer = "X"
+    
+    def checkWin(self):
+        global gameRunning
+        if self.checkDiag() or self.checkHorizontal() or self.checkRow():
+            self.printboard()
+            print("GAME OVER")
+            print(f"The winner is {winner}")
+            gameRunning = False
 
 instance1 = TicTacToe()
 
 def refresh_screen():
     instance1.printboard()
 
-while True:
+def startGame():
+    global gameRunning
+    print("Select your desired position with this index:")
+    print("BOARD POSITION")
+    print("1", " | ", "2", " | ", "3")
+    print("---------------")
+    print("4", " | ", "5", " | ", "6")
+    print("---------------")
+    print("7", " | ", "8", " | ", "9")
+    print("---------------")
+    print("")
+    if input("Are you ready to start the game ? (Y/N): ") == "Y":
+        print("GAME STARTED")
+        print("")
+        gameRunning = True
+    else:
+        gameRunning = False
+
+startGame()
+
+while gameRunning:
     refresh_screen()
     instance1.playerInput()
-    refresh_screen()
+    instance1.checkWin()
+    instance1.checkTie()
+    instance1.switchPlayer()
